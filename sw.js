8e4cd48473d5cb1e56e6dc4cf4acf7e278244919
Shinononome-Enana <galaxyerkw2023@outlook.com>
2024-10-25 21:33:11 +0900

    Site updated: 2024-10-25 21:33:10

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1729859453783";
+const VERSION = "1729859576913";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
